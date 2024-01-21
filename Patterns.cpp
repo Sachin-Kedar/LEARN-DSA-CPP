f8fe9d5b215d0eb19f0133483ca8b71c6a15efb7
sachin kedar <kedarsachin1020@gmail.com>
2024-01-22 00:49:04 +0530

    codes

--- Patterns.cpp
+++ Patterns.cpp
@@ -299,7 +299,7 @@ int main (){
     }
 }
 
-*/
+
 //   using While Loop
 
 #include <iostream>
@@ -317,3 +317,135 @@ int main (){
         i++;
     }
 }
+
+--  Hologram with * 
+****
+*  *
+*  *
+*  *
+****
+
+#include <iostream>
+using namespace std;
+
+int main (){
+    int row=5;
+    int col=4;
+    for (int i =1;i<=row;i++){
+        for (int j=1;j<=col;j++){
+            if (i==1 || i==row || j==1 ||j==col){
+                cout <<"*";
+            }
+            else{
+                cout<<" ";
+            }
+        }
+        cout<<endl;
+    }
+}
+
+
+---  Inverted Half Pyramid
+*****
+****
+***
+**
+*
+
+
+#include <iostream>
+using namespace std;
+
+int main (){
+    int n=5;
+    while(n>0){
+        for (int i=0;i<n;i++){
+            cout<<"*";
+        }
+        cout<<endl;
+        n--;
+    }
+}
+
+-- Half Pyramid after 180 degree rotation
+    *
+   **
+  ***
+ ****
+*****
+
+
+
+
+#include <iostream>
+using namespace std;
+
+int main (){
+    int n=5;
+    for(int i=1;i<=n;i++){
+        for (int j=1;j<=n;j++){
+            if (j <= n-i){
+                cout<<" ";
+
+            }
+            else{
+                 cout<<"*";
+            }
+
+           
+        }
+        cout<<endl;
+    }
+}
+
+
+1 2 3 4 5 
+1 2 3 4
+1 2 3
+1 2
+1
+
+#include <iostream>
+using namespace std;
+
+int main (){
+    int n=5;
+    for(int i=0;i<n;i++){
+        int count=1;
+        for (int j=1;j<=n;j++){
+            if (j < n+1-i){
+            cout<<count<<" ";
+            count++;
+            }
+        }
+        cout<<endl;
+    }
+}
+
+1
+01
+101
+0101
+10101
+
+//  here in this pattern where i+j is even value is 1 else its 0.
+
+#include <iostream>
+using namespace std;
+
+int main (){
+    int n=5;
+    for (int i =1;i<=n;i++){
+        for (int j=1;j<=i;j++){
+            if((i+j)%2==0){
+                cout<<"1";
+            }
+            else{
+                cout<<"0";
+            }
+        }
+        cout<<endl;
+    }
+}
+
+*/
