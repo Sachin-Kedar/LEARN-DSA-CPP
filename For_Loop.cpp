c8faa72df37b02f28d2f11f90925b3fe84bea8e7
sachin kedar <kedarsachin1020@gmail.com>
2024-01-22 02:00:20 +0530

    codes

--- For_Loop.cpp
+++ For_Loop.cpp
@@ -112,5 +112,29 @@ int main(){
 
 }
 
+#include<iostream>
+#include <cmath>
+using namespace std;
+
+int main(){
+    int N ;
+    cout <<"Enter a Number : ";
+    cin>>N;
+    int temp = N;
+    int cnt = 0;
+    while (temp!= 0) {
+        int rem = temp % 10;
+        cnt = cnt + pow(rem, 3);  // Corrected the calculation
+        temp = temp / 10;         // Added the division operation
+    }
+
+    if (cnt == N){
+        cout << "This is an Armstrong number" << endl;
+    } else {
+        cout << "This is not an Armstrong number" << endl;
+    }  
+
+    return 0;  // Added return statement
+}
 
 
