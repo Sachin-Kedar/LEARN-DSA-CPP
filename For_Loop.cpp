8b7290d7a8d539c207e7fe866ffa7da3e1d00fd4
sachin kedar <kedarsachin1020@gmail.com>
2024-01-22 01:59:34 +0530

    codes

--- For_Loop.cpp
+++ For_Loop.cpp
@@ -40,6 +40,32 @@ int main(){
     
 }
 
+//  We can check the loop only till sqrt(number)  -- if any number is divisible by its sqrt then it will be not Prime number  
+#include<iostream>
+#include <cmath>
+using namespace std;
+
+int main(){
+    int N;
+    cout<< "Enter the number N: ";
+    cin>> N;
+    bool isPrime=1;
+    for (int i=2;i<=sqrt(N);i++){
+        if (N%i==0){
+            isPrime=0;
+            break;
+        }
+    }
+    if (isPrime==0){
+        cout<<"This is not Prime number"<<endl;
+        }
+    else {
+        cout<<"This is a Prime number"<<endl;
+    }  
+    
+}
+//  sqrt(49) --- 7
+
 //  Q3-- Reversed Number
 
 
--- new.cpp
+++ new.cpp
@@ -1,11 +1,24 @@
 #include<iostream>
+#include <cmath>
 using namespace std;
+
 int main(){
-    int arr[5]={1,2,3,45};
-    int i=4;
-    while(i>0){
-        cout <<arr[i]<<" ";
-        i--;
+    int N ;
+    cout <<"Enter a Number : ";
+    cin>>N;
+    int temp = N;
+    int cnt = 0;
+    while (temp!= 0) {
+        int rem = temp % 10;
+        cnt = cnt + pow(rem, 3);  // Corrected the calculation
+        temp = temp / 10;         // Added the division operation
     }
 
+    if (cnt == N){
+        cout << "This is an Armstrong number" << endl;
+    } else {
+        cout << "This is not an Armstrong number" << endl;
+    }  
+
+    return 0;  // Added return statement
 }
