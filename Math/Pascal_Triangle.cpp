10e5cd34045704f6d651000159ed8c7c64eee8c3
sachin kedar <kedarsachin1020@gmail.com>
2024-01-22 19:01:06 +0530

    codes

---
+++ Math/Pascal_Triangle.cpp
@@ -0,0 +1,30 @@
+/* Pascal triangle is basically depends on nCr method.
+            //    nCr = n!/(r!*(n-r)!)
+
+        1-0c0
+        1 1 
+        1 2 1 
+        1 3 3 1 
+        1 4 6 4 1 ---4 will be from  4C1 = 4! / (1!*3!)  // all values will be like this only.
+*/
+#include <iostream>
+using namespace std;
+int fact(int n){
+    int factorial=1;
+    for(int i=1;i<=n;i++){
+        factorial*=i;
+    }
+    return factorial;
+}
+int main(){
+    int n;
+    cin>>n;
+
+    for (int i=0;i<n;i++){
+        for (int j=0;j<=i;j++){
+            cout<<fact(i)/(fact(j)*fact(i-j))<<" ";
+        }
+        cout<<endl;
+    }
+
+}
