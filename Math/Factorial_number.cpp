c1f45b89ce0cd9c2085477182822c51169888c2c
sachin kedar <kedarsachin1020@gmail.com>
2024-01-22 18:15:30 +0530

    codes

--- Math/Factorial_number.cpp
+++ Math/Factorial_number.cpp
@@ -1,22 +1,17 @@
 #include<iostream>
+#include <cmath>
 using namespace std;
-
-void fib(int n){
-    int t1=0;
-    int t2=1;
-    int nextTerm;
+int fact(int n){
+    int factorial=1;
     for (int i=1;i<=n;i++){
-        cout<<t1<<endl;
-        nextTerm=t1+t2;
-        t1=t2;
-        t2=nextTerm;
+        factorial*=i;
     }
-    return;
+    return factorial;
 }
 int main(){
-    int n;
-    cin>>n;
-
-    fib(n);
-    return 0;
+    int N ;
+    cout <<"Enter a Number : ";
+    cin>>N;
+    int ans = fact(N);
+    cout<<ans<<endl;
 }
--- new.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-#include <cmath>
-using namespace std;
-
-int main(){
-    int N ;
-    cout <<"Enter a Number : ";
-    cin>>N;
-}
