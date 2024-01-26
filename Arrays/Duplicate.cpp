e65881234a9bc340e537a5b1c4acc6cb2119ebb9
sachin kedar <kedarsachin1020@gmail.com>
2024-01-27 01:33:44 +0530

    codes

--- Arrays/Duplicate.cpp
+++ Arrays/Duplicate.cpp
@@ -1,5 +1,7 @@
 /*
-You are given an array ‘ARR’ of size ‘N’ containing each number between 1 and ‘N’ - 1 at least once. There is a single integer value that is present in the array twice. Your task is to find the duplicate integer value present in the array.
+You are given an array ‘ARR’ of size ‘N’ containing each number between 1 and ‘N’ - 1 at least once. 
+There is a single integer value that is present in the array twice. 
+Your task is to find the duplicate integer value present in the array.
 
 For example:
 
---
+++ Arrays/Majority_Element.cpp
@@ -0,0 +1,80 @@
+/*
+
+Leet Code
+
+Problem Statement-->>  Majority Element
+
+Given an array nums of size n, return the majority element.
+The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+
+Example 1:
+Input: nums = [3,2,3]
+Output: 3
+
+Example 2:
+Input: nums = [2,2,1,1,1,2,2]
+Output: 2
+*/
+
+#include <iostream>
+#include<vector>
+using namespace std;
+
+int majorityElement(int nums[],int n){
+    int j;
+    for (int i = 0; i < n; i++)
+    {
+        int cnt = 0;
+        for (j = i; j < n; j++)
+        {
+            if (nums[i] == nums[j])
+            {
+                cnt++;
+            }
+            if (cnt > n / 2)
+            {
+                return nums[j];
+            }
+        }
+    }
+    return 0;
+}
+int main(){
+    int n=3;
+    int nums[n] = {3, 2, 3};
+    cout<< majorityElement(nums,n)<<endl;
+}
+
+
+// We can write using vector array like below: 
+/*
+#include <iostream>
+#include<vector>
+using namespace std;
+int majorityElement(vector <int> nums){
+    int n=nums.size();
+    int j;
+    for (int i = 0; i < n; i++)
+    {
+        int cnt = 0;
+        for (j = i; j < n; j++)
+        {
+            if (nums[i] == nums[j])
+            {
+                cnt++;
+            }
+            if (cnt > n / 2)
+            {
+                return nums[j];
+            }
+        }
+    }
+    return 0;
+}
+int main(){
+    vector <int> nums = {2, 2, 1, 1, 1, 2, 2};
+    int ans = majorityElement(nums);
+    cout<<ans<<endl;
+    return 0;
+}
+*/
