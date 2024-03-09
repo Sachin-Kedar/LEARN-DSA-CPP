b6ad4355ac328a47b1a8913c79d22ac8670f938b
Sachin kedar <kedarsachin1020@gmail.com>
2024-03-10 00:36:23 +0530

    Codes

--- String/Sorting_string.cpp
+++ String/Sorting_string.cpp
@@ -1,9 +1,27 @@
 #include<bits/stdc++.h>
-
 using namespace std;
 
-int main(){
-    string str = "welcome";
-    sort(str.begin(),str.end());
-    cout<<str<<endl;
+//  sort string without using sort function
+
+string countSorting(string str){
+    vector<int> freq(26,0);
+    for (int i=0; i<str.length();i++){
+        int index = str[i]  - 'a';
+        freq[index]++;
+    }
+    int j=0;
+    for(int i=0;i<26;i++){
+        while(freq[i]--){
+            str[j++]=i+'a';
+        }
+    }
+    return str;
+}
+int main (){
+    string str ;
+    //sort(str.begin(),str.end());
+    //cout<<str<<endl;
+    cin>> str;
+    cout<<countSorting(str)<<endl;
+    return 0;
 }
